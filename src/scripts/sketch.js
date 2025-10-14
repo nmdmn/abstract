4438419fd821f3762eb5724cd06ee12ac12a81e7
nmdmn <afarago.nmd@gmail.com>
2025-10-14 19:02:57 +0200

    ADD basic camera animation section by section

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -1,4 +1,5 @@
 import * as Dat from "dat.gui";
+import {Tween, Group} from "@tweenjs/tween.js";
 import * as Three from "three";
 import {Vector3} from "three";
 
@@ -45,28 +46,41 @@ export default class Sketch {
 
     this.camera = new Three.PerspectiveCamera(33, window.innerWidth / window.innerHeight, .1, 1000.);
     this.camera.lookAt(new Vector3(0, 0, 0));
-    this.camera.position.copy(new Vector3(0, 0, 99));
+    this.camera.position.copy(new Vector3(0, 0, 33));
     this.app = new App(canvas, this.camera);
 
     this.box = new Box(this.app, ui);
     this.grid = new Grid(this.app, ui);
     this.grid.mesh.position.copy(new Vector3(0, 0, -10));
 
+
+    const animations = new Group();
+
     document.querySelector("main").addEventListener("scrollsnapchanging", (event) => {
+      animations.removeAll();
       switch (event.snapTargetBlock.id) {
         case "section-0":
-          console.log("section 0");
+          animations.add(new Tween(this.camera.position)
+            .to(new Vector3(0, 0, 33), 500)
+            .start());
           break;
         case "section-1":
-          console.log("section 1");
+          animations.add(new Tween(this.camera.position)
+            .to(new Vector3(0, 0, 99), 500)
+            .start());
           break;
         case "section-2":
-          console.log("section 2");
+          animations.add(new Tween(this.camera.position)
+            .to(new Vector3(-5, 0, 66), 500)
+            .start());
           break;
         case "section-3":
-          console.log("section 3");
+          animations.add(new Tween(this.camera.position)
+            .to(new Vector3(0, 10, 99), 500)
+            .start());
           break;
       }
+      console.log(animations);
     });
 
     this.app.addKeydownCallbacks((event) => {
@@ -77,7 +91,8 @@ export default class Sketch {
       }
     });
 
-    this.app.addUpdateCallback(() => {
+    this.app.addUpdateCallback((deltaTime, time) => {
+      animations.update();
       this.app.renderer.toneMappingExposure = Math.pow(ui.exposure.value, 4);
       this.app.bloomPass.threshold = ui.threshold.value;
       this.app.bloomPass.strength = ui.strength.value;
