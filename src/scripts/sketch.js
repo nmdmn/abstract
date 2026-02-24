d633fe297ec88be679fd35bc463c0455a2576d4d
nmdmn <afarago.nmd@gmail.com>
2026-02-24 09:55:25 +0100

    MOD remove member useless member variables

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -15,9 +15,9 @@ export default class Sketch {
 
     const mouse = new Three.Vector2(window.innerWidth / 2., window.innerHeight / 2.);
 
-    this.camera = new Three.OrthographicCamera(-1, 1, 1, -1, 0, 1)
-    this.app = new App(canvas, this.camera);
-    this.uniforms = {
+    const camera = new Three.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+    const app = new App(canvas, camera);
+    const uniforms = {
       general: {
         value: {
           elapsedTime: 0,
@@ -43,15 +43,15 @@ export default class Sketch {
         drawBuffers: false,
         haderTextureLOD: false,
       },
-      uniforms: this.uniforms,
+      uniforms: uniforms,
       vertexShader: VertexShader,
       fragmentShader: FragmentShader,
     });
 
-    this.mesh = new Three.Mesh(geometry, material);
-    this.app.scene.add(this.mesh);
+    const mesh = new Three.Mesh(geometry, material);
+    app.scene.add(mesh);
 
-    this.app.addKeydownCallbacks((event) => {
+    app.addKeydownCallbacks((event) => {
       switch (event.key) {
         case "Escape":
           Dat.GUI.toggleHide();
@@ -64,13 +64,13 @@ export default class Sketch {
       mouse.y = window.innerHeight - event.clientY;
     });
 
-    this.app.addUpdateCallback((deltaTime, elapsedTime) => {
-      this.uniforms.general.value.elapsedTime = elapsedTime;
-      this.uniforms.general.value.deltaTime = deltaTime;
-      this.uniforms.general.value.mouse.copy(mouse);
-      this.uniforms.general.value.resolution = new Three.Vector2(window.innerWidth, window.innerHeight);
+    app.addUpdateCallback((deltaTime, elapsedTime) => {
+      uniforms.general.value.elapsedTime = elapsedTime;
+      uniforms.general.value.deltaTime = deltaTime;
+      uniforms.general.value.mouse.copy(mouse);
+      uniforms.general.value.resolution = new Three.Vector2(window.innerWidth, window.innerHeight);
     });
 
-    this.app.start();
+    app.start();
   }
 }
