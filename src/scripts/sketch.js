3e4f8399b852b1831d3df5831899cff9807e222c
nmdmn <afarago.nmd@gmail.com>
2026-02-24 10:40:49 +0100

    MOD rename shader

--- src/scripts/sketch.js
+++ src/scripts/sketch.js
@@ -3,8 +3,8 @@ import * as Three from "three";
 
 import { App, UI } from "./app.js";
 
-import VertexShader from "./shaders/ophanim/vOphanim.glsl"
-import FragmentShader from "./shaders/ophanim/fOphanim.glsl"
+import VertexShader from "./shaders/basic/v_basic.glsl.glsl"
+import FragmentShader from "./shaders/basic/f_basic.glsl.glsl"
 
 const ui = {
 };
